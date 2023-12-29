94e4695fc2f30a0e4456aeedb467b5407f179270
Wendy <wendyrandolph44@gmail.com>
2023-12-28 19:07:41 -0700

    created modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,31 @@
-import '../styles/styles.css'; 
-import jquery from 'jquery';
+import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll'; 
-import StickyHeader from './modules/StickyHeader'; 
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
-let stickyHeader = new StickyHeader(); 
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75); 
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60); 
 
-let mobileMenu = new MobileMenu(); 
+
+let stickyHeader = new StickyHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+let mobileMenu = new MobileMenu();
+
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import('./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
+
 
 if (module.hot) {
     module.hot.accept()
@@ -16,4 +33,4 @@ if (module.hot) {
 
 
 
- 
+
