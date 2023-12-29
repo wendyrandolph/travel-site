a8493789e4cac1c7f4e7be212f0f042133ed61f2
Wendy <wendyrandolph44@gmail.com>
2023-12-29 11:12:12 -0700

    Continued Optimization

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,6 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-
-
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,89 @@
-const path = require('path'); 
+const currentTask = process.env.npm_lifecycle_event
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
-const postCSSPlugins = [ 
-   require('postcss-import'),
-   require('postcss-mixins'),
-   require('postcss-simple-vars'), 
-   require('postcss-nested'), 
-   require('postcss-hexrgba'),
-   require('autoprefixer') 
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-hexrgba'),
+    require('autoprefixer')
 ]
-module.exports = {
-    entry: "./app/assets/scripts/App.js",
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: { 
-        watchFiles: ["app/**/*.html"], 
-        static: { 
-            directory: path.join(__dirname, "app"), 
-            watch: false
-        }, 
-        hot: true, 
-        port: 3000, 
-        
-    },
-    mode: 'development',
+class RunAfterCompile{ 
+    apply(compiler){ 
+        compiler.hooks.done.tap('Copy images', function() { 
+            fse.copySync('./app/assets/images', './dist/assets/images')
+        })
+    }
+}
 
+let cssConfig = {
+    test: /\.css$/i,
+    use: [{ loader: "css-loader", options: { url: false } },
+    { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }
+    ]
+
+}
+let pages = fse.readdirSync('./app').filter(function (file){ 
+    return file.endsWith('.html')
+}).map(function(page){ 
+    return new HtmlWebpackPlugin({
+        filename: page, 
+        template: `./app/${page}`
+    })
+})
+
+let config = {
+    entry: "./app/assets/scripts/App.js",
+    plugins: pages, 
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
-               
-        }
+            cssConfig
         ]
     }
 }
+
+if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader')
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    }
+    config.devServer = {
+        watchFiles: ["./app/**/*.html"],
+        static: {
+            directory: path.join(__dirname, "app"),
+            watch: false
+        },
+        hot: false,
+        open: true, 
+        port: 3000,
+    },
+        config.mode = 'development'
+}
+
+if (currentTask == "build") {
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+        config.mode = "production",
+        config.optimization = {
+            splitChunks: { chunks: 'all' },
+            minimizer: [`...`, new CssMinimizerPlugin()]
+        }
+    config.plugins.push(
+        new MiniCssExtractPlugin({ filename: `styles.[hash].css` }),
+        new RunAfterCompile()
+    )
+};
+
+
+module.exports = config
